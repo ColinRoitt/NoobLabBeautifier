a4962c9bb7bc49789cec314d5884f9ca94956813
cooltennis01 <cooltennis@gmail.com>
2016-12-12 23:51:51 +0000

    init

---
+++ NoobLabNicifier.js
@@ -0,0 +1,9 @@
+//alert("This is noob lab");
+
+var buttonList = document.getElementsByTagName("Button");
+buttonList[0].click();
+
+document.getElementById("content").style.width = "50%";
+document.getElementById("editor-wrapper").style.width = "50%";
+document.getElementById("toolbar").style.width = "50%";
+document.getElementById("output-outer").style.width = "50%";
